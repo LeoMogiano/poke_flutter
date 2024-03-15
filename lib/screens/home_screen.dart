a6827c7aa822827111f78b28d14e229ffd6e22ab
Leo Mogiano <leomogiano@outlook.com>
2024-03-14 22:37:43 -0400

    Update HomeScreen and dependencies versions

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:examen_server/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
